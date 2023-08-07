b36974ea73f4ba80b3d56e00a17f97861cc00786
Aiman Azad khan <92166287+aimanazadkhan@users.noreply.github.com>
2023-08-07 17:31:40 +0600

    header.php

--- NexusHealth/Blood Bank/header.php
+++ NexusHealth/Blood Bank/header.php
@@ -1,4 +1,4 @@
-<header class="p-3">
+<header class="p-3 position-relative">
     <div class="container">
         <div class="d-flex flex-wrap align-items-center justify-content-between justify-content-lg-start">
             <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -8,7 +8,7 @@
                 <li><a href="#" class="nav-link px-2 text-danger">About</a></li>
             </ul>
 
-            <div class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
+            <div class="position-absolute top-50 start-50 translate-middle">
                 <a href="../Homepage/index.php">
                     <img src="../logo/reglogo.png" alt="logo1" style="width: 250px;">
                 </a>

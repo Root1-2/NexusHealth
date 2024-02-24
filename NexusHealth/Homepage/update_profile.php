af1003450a5f8854c53ac0acc45f7ee1dd5619f9
Aiman Azad khan <92166287+aimanazadkhan@users.noreply.github.com>
2024-02-24 23:00:45 +0600

    Update update_profile.php

--- NexusHealth/Homepage/update_profile.php
+++ NexusHealth/Homepage/update_profile.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 include "../Database/connection.php";
+include "../Database/sessionUserData.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if (isset($_POST['updateProfile'])) {
     $firstName = $_POST['firstName'];
     $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
     $phone = $_POST['phone'];
     $dob = $_POST['dob'];
     $weight = $_POST['weight'];
@@ -15,8 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Repeat similar lines to get other form fields
 
     // Prepare the SQL statement
-    $update_profile = "UPDATE `register` SET `firstName`='$firstName',`lastName`='$lastName',`email`='$email',`pNumber`='$phone',`dob`='$dob',`weight`='$weight',`height`='$height',`address`='$address',`city`='$city' WHERE userName='{$_SESSION['userName']}'";
-
+    $update_profile = "UPDATE `register` SET `firstName`='$firstName',`lastName`='$lastName',`pNumber`='$phone',
+        `dob`='$dob',`weight`='$weight',`height`='$height',`address`='$address',`city`='$city' WHERE userName='{$row['userName']}'";
     // Execute the SQL statement
     if (mysqli_query($conn, $update_profile)) {
         // Profile updated successfully
@@ -27,4 +27,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Error: " . mysqli_error($conn);
     }
 }
+
+if (isset($_FILES['photo'])) {
+    $tempLoc = $_FILES['photo']['tmp_name'];
+    $imgName = $_FILES['photo']['name'];
+    $imageDestination = "../ProfilePictures/" . $imgName;
+
+    move_uploaded_file($tempLoc, $imageDestination);
+
+    $query = mysqli_query($conn, "UPDATE `register` SET `profilepic`='$imageDestination' WHERE userName = '{$row['userName']}'");
+    header("Location: profile.php");
+}
+
+if (isset($_POST['notificationSwitch'])) {
+    if ($_POST['notificationSwitch'] == 'on') {
+        // On-Off
+
+        $update = mysqli_query($conn, "UPDATE donor_list SET status='Unavailable' WHERE Name = '{$_SESSION['fullName']}'");
+        header("Location: profile.php");
+
+    } else {
+        // Off->On
+
+        $query0 = mysqli_query($conn, "SELECT * FROM donor_list WHERE (Name = '{$_SESSION['fullName']}' AND `Phone number`= '{$row['pNumber']}' AND status='Unavailable')");
+        $query1 = mysqli_query($conn, "SELECT * FROM donor_list WHERE (Name = '{$_SESSION['fullName']}' AND `Phone number`= '{$row['pNumber']}')");
+        if (mysqli_num_rows($query0) > 0) {
+            $update = mysqli_query($conn, "UPDATE donor_list SET status='Available' WHERE Name = '{$_SESSION['fullName']}'");
+            header("Location: profile.php");
+        } else if (!mysqli_num_rows($query1) > 0) {
+            $query2 = mysqli_query($conn, "INSERT INTO `donor_list`(`Name`, `BloodGroup`, `Phone number`, `Address`, `status`) 
+            VALUES ('{$_SESSION['fullName']}','{$row['bloodGroup']}','{$row['pNumber']}','{$row['address']}','Available')");
+            header("Location: profile.php");
+        } else {
+            echo "2";
+        }
+    }
+} else {
+    $update = mysqli_query($conn, "UPDATE donor_list SET status='Unavailable' WHERE Name = '{$_SESSION['fullName']}'");
+    header("Location: profile.php");
+}
+
+
 ?>

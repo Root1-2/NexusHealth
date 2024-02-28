3cbcef740c3f5c9ee7852d421de2f9911b54250a
John Doe <abdullahalmasrur8@gmail.com>
2024-02-28 14:12:21 +0600

    about.php link in index.php

--- NexusHealth/Homepage/index.php
+++ NexusHealth/Homepage/index.php
@@ -73,9 +73,7 @@ include "../Database/connection.php";
 
                         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                             <li><a href="#features" class="nav-link px-2 text-dark">Features</a></li>
-                            <li><a href="#" class="nav-link px-2 text-dark">Pricing</a></li>
-                            <li><a href="#" class="nav-link px-2 text-dark">FAQs</a></li>
-                            <li><a href="#" class="nav-link px-2 text-dark">About</a></li>
+                            <li><a href="about.php" class="nav-link px-2 text-dark">About</a></li>
                         </ul>
 
 

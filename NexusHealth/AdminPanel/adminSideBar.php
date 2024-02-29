b3e5fde9a743bd3e20f161a0f0ce7fcfff3c7883
John Doe <abdullahalmasrur8@gmail.com>
2024-03-01 02:09:53 +0600

    #

--- NexusHealth/AdminPanel/adminSideBar.php
+++ NexusHealth/AdminPanel/adminSideBar.php
@@ -22,9 +22,8 @@
     }
 </style>
 
-<div class="ps-3 py-3 border-end sidebar" id="side_nav"
-    style="width: 16rem; height: 100vh; background-color: #222e3c;">
-    <div class="pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
+<div class="ps-3 py-3 border-end sidebar" id="side_nav" style="width: 16rem; height: 100vh; background-color: #222e3c;">
+    <div class="pb-3 mb-3  text-decoration-none border-bottom">
         <div class="d-flex flex-wrap">
             <i class="fa-solid fa-user-gear fs-4"></i>
             <p class="ms-3 fs-5 fw-semibold">Admin Panel</p>
@@ -47,74 +46,51 @@
             <li class="border-top mb-3"></li>
 
             <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded border-0 collapsed"
-                    data-bs-toggle="collapse" data-bs-target="#blood-collapse">
-                    <span><i class="fa-solid fa-droplet me-3"></i>Blood Bank</span>
+                <button class="btn btn-toggle align-items-center rounded border-0 collapsed" data-bs-toggle="collapse"
+                    data-bs-target="#blood-collapse">
+                    <span class="link-danger"><i class="fa-solid fa-droplet me-3"></i>Blood Bank</span>
                 </button>
-                <div class="collapse <?php if (isset($bloodbank))
+                <div class="p-1 collapse <?php if (isset($bloodbank))
                     echo "show"; ?>" id="blood-collapse">
-                    <ul class="btn-toggle-nav list-unstyled fw-normal px-4 small">
-                        <li><a href="blood-donorList.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Donor
-                                    List</span></a></li>
-                        <li><a href="blood-addBlood.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Add
-                                    Donor</span></a></li>
+                    <ul class="btn-toggle-nav list-unstyled fw-normal px-3 small">
+                        <li class="mb-2"><a href="blood-donorList.php" class="p-2 text-decoration-none rounded">
+                                <span class="link-danger"><i class="fa-regular me-2 fa-circle-dot"></i>Donor List</span></a></li>
+                        <li><a href="blood-addBlood.php" class="p-2 text-decoration-none rounded">
+                            <span class="link-danger"><i class="fa-regular me-2 fa-circle-dot"></i>Add Donor</span></a></li>
                     </ul>
                 </div>
             </li>
 
             <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded border-0 collapsed"
-                    data-bs-toggle="collapse" data-bs-target="#orders-collapse">
-                    <span><i class="fa-solid fa-user-doctor me-3"></i>Doctor Appointment</span>
+                <button class="btn btn-toggle align-items-center rounded border-0 collapsed" data-bs-toggle="collapse"
+                    data-bs-target="#orders-collapse">
+                    <span class="link-primary"><i class="fa-solid fa-user-doctor me-3"></i>Doctor Appointment</span>
                 </button>
-                <div class="collapse <?php if (isset($doctorappointment))
+                <div class="p-1 collapse <?php if (isset($doctorappointment))
                     echo "show"; ?>" id="orders-collapse">
-                    <ul class="btn-toggle-nav list-unstyled fw-normal px-4 small">
-                        <li><a href="doc-docList.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Doctor
-                                    List</span></a></li>
-                        <li><a href="doc-appList.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Appointments
-                                    List</span></a></li>
-                        <li><a href="doc-docAdd.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Add
-                                    Doctors</span></a></li>
+                    <ul class="btn-toggle-nav list-unstyled fw-normal px-3 small">
+                        <li class="mb-2"><a href="doc-docList.php" class="p-1 text-decoration-none rounded">
+                            <span class="link-primary"><i class="fa-regular me-2 fa-circle-dot"></i>Doctor List</span></a></li>
+                        <li class="mb-2"><a href="doc-appList.php" class="p-1 text-decoration-none rounded">
+                            <span class="link-primary"><i class="fa-regular me-2 fa-circle-dot"></i>Appointments List</span></a></li>
+                        <li><a href="doc-docAdd.php" class="p-1 text-decoration-none rounded">
+                            <span class="link-primary"><i class="fa-regular me-2 fa-circle-dot"></i>Add Doctors</span></a></li>
                     </ul>
                 </div>
             </li>
 
             <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded border-0 collapsed"
-                    data-bs-toggle="collapse" data-bs-target="#med-collapse">
-                    <span><i class="fa-solid fa-pills me-3"></i>Med Corner</span>
+                <button class="btn btn-toggle align-items-center rounded border-0 collapsed" data-bs-toggle="collapse"
+                    data-bs-target="#med-collapse">
+                    <span class="link-success"><i class="fa-solid fa-pills me-3"></i>Med Corner</span>
                 </button>
-                <div class="collapse <?php if (isset($medcorners))
+                <div class="p-1 collapse <?php if (isset($medcorners))
                     echo "show"; ?>" id="med-collapse" style="">
-                    <ul class="btn-toggle-nav list-unstyled fw-normal px-4 small">
-                        <li><a href="med-addMed.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Add
-                                    Medicine</span></a></li>
-                        <li><a href="med-medList.php"
-                                class="link-body-emphasis p-1 text-decoration-none rounded"><span>Medicine
-                                    List</span></a></li>
-                    </ul>
-                </div>
-            </li>
-
-            <li class="border-top my-3"></li>
-
-            <li class="mb-1">
-                <button class="btn btn-toggle align-items-center rounded border-0 collapsed"
-                    data-bs-toggle="collapse" data-bs-target="#users-collapse">
-                    <span><i class="fa-solid fa-users-rays me-3"></i>Users</span>
-                </button>
-                <div class="collapse  <?php if (isset($profile))
-                    echo "show"; ?>" id="users-collapse" style="">
-                    <ul class="btn-toggle-nav list-unstyled fw-normal px-4 small">
-                        <li><a href="#"
-                                class="d-inline-flex p-1 text-decoration-none"><span>Profiles</span></a></li>
+                    <ul class="btn-toggle-nav list-unstyled fw-normal px-3 small">
+                        <li class="mb-2"><a href="med-addMed.php" class=" p-1 text-decoration-none rounded">
+                                <span class="link-success"><i class="fa-regular me-2 fa-circle-dot"></i>Add Medicine</span></a></li>
+                        <li><a href="med-medList.php" class=" p-1 text-decoration-none rounded">
+                                <span class="link-success"><i class="fa-regular me-2 fa-circle-dot"></i>Medicine List</span></a></li>
                     </ul>
                 </div>
             </li>
@@ -124,7 +100,7 @@
             <li class="mb-1">
                 <a href="../Homepage/" class="text-decoration-none">
                     <button class="btn rounded border-0">
-                        <span><i class="fa-solid fa-users-rays me-3"></i>NexusHealth</span>
+                        <span class="link-info"><i class="fa-solid fa-users-rays me-3"></i>NexusHealth</span>
                     </button>
                 </a>
             </li>

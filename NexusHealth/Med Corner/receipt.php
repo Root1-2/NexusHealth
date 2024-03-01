ab0bb60755298aec8130be3aa99c5098dbc79d04
John Doe <abdullahalmasrur8@gmail.com>
2024-03-01 16:30:31 +0600

    receipt item fixed

--- NexusHealth/Med Corner/receipt.php
+++ NexusHealth/Med Corner/receipt.php
@@ -19,28 +19,23 @@ include "../Database/sessionUserData.php";
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-
     <!-- Bootstrap Icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css"
         integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="styles.css">
+    <!-- DataTable -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" />
     <style>
         .custom-container {
             outline: 1px solid;
-            /* Add outline to the entire div */
             padding: 10px;
-            /* Add some padding for better appearance */
             display: inline-block;
-            /* Keep the div inline with its contents */
         }
 
         .custom-container i {
             font-size: 1.5rem;
             vertical-align: middle;
-            /* Align the icon vertically in the middle */
             margin-right: 5px;
-            /* Add some space between the icon and the button */
         }
 
         .dashicon {
@@ -56,66 +51,59 @@ include "../Database/sessionUserData.php";
 <body>
 
     <div class="container-fluid">
-    <header class="p-3 position-relative">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-between justify-content-lg-start">
-            
-            <div class="">
-                <a href="../Med Corner/medhome.php">
-                    <img src="../logo/reglogo.png" alt="logo1" style="width: 250px;">
-                </a>
+        <header class="p-3 position-relative">
+            <div class="container">
+                <div class="d-flex flex-wrap align-items-center justify-content-between justify-content-lg-start">
+
+                    <div class="">
+                        <a href="../Med Corner/medhome.php">
+                            <img src="../logo/reglogo.png" alt="logo1" style="width: 250px;">
+                        </a>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</header>
-        
+        </header>
+
 
         <div class="px-5 py-3" style="width: 100%;">
             <div>
-                
-                
                 <span class="display-5 ms-5 px-3">Recipts</span>
                 <i class="bi bi-receipt dashicon display-6 me-2"></i>
                 <hr class="bg-primary">
             </div>
 
-            <div class="container mt-5">
-                <table id="appointTable" class="table align-middle mb-0 bg-white">
+            <div class="container mt-3">
+                <table id="dataTable" class="table align-middle mb-0">
                     <thead class="bg-light">
                         <tr>
-                            <th>Order id</th>
-                            <th>Order item</th>
-                            
-
+                            <th>Order ID</th>
+                            <th>Order Item</th>
                         </tr>
                     </thead>
                     <tbody>
+
                         <?php
-
-
                         $buyerUsername = $_SESSION['userName'];
                         $receiptdata = mysqli_query($conn, "SELECT * FROM `receipt` WHERE `orderBuyer` = '$buyerUsername'");
 
-
                         while ($row2 = mysqli_fetch_array($receiptdata)) {
-                            echo "
-                            <tr>
-                                 <td>
-                                    <div class='d-flex align-items-center'>
-                                <div>
-                                    <p class='fw-medium mb-1'>" . $row2['orderID'] . " </p>
-                                  
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                        <p class='text-muted mb-0'>" . $row2['items'] . " </p>
-                          
-                        </td>
-
-                       
-
-                    </tr> ";
+                            $items = json_decode($row2['items'], true);
+                            echo "<tr>";
+                            echo "<td><p class='fw-medium mb-1'>" . $row2['orderID'] . "</p></td>";
+                            echo "<td>";
+                            foreach ($items as $key => $item) {
+                                $medName = $item['medName'];
+                                $quantity = $item['quantity'];
+                                $medPrice = $item['medPrice'];
+                                echo "
+                                    <div class='mb-2'>
+                                        <p class='fw-medium mb-1'>" . ($key + 1) . ". Medicine Name: $medName</p>
+                                        <p class='text-muted mb-0'>Quantity: $quantity</p>
+                                        <p class='text-muted mb-0'>Price: $medPrice</p>
+                                    </div>";
+                            }
+                            echo "</td>";
+                            echo "</tr>";
                         }
 
                         ?>
@@ -126,10 +114,23 @@ include "../Database/sessionUserData.php";
         </div>
     </div>
 
+    <!-- DataTable -->
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
         crossorigin="anonymous"></script>
 
+    <script>
+        // DataTable Initialization
+        $(document).ready(function () {
+            $('#dataTable').DataTable({
+                "lengthMenu": [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+                "pageLength": 5 
+            });
+        });
+    </script>
 </body>
 
 </html>

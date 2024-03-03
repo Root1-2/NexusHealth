3b1c34f7bd79b8c755d0a6eb97175a25c380295c
John Doe <abdullahalmasrur8@gmail.com>
2024-03-03 10:08:54 +0600

    #

--- NexusHealth/AdminPanel/docDataFetch.php
+++ NexusHealth/AdminPanel/docDataFetch.php
@@ -17,12 +17,12 @@ if (isset($_POST['rowId'])) {
         echo json_encode($data);
     } else {
         // Handle the case where the query fails
-        header('Server Error');
+        header('HTTP/1.1 500 Internal Server Error');
         echo json_encode(['error' => 'Failed to fetch data']);
     }
 } else {
     // Handle the case where rowId is not set
-    header('Server Error');
+    header('HTTP/1.1 400 Bad Request');
     echo json_encode(['error' => 'rowId not provided in the request.']);
 }
 
